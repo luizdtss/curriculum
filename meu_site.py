aa93b7a38671533045205d2f311edc157c4dd5ae
luizedtss <luiz.dtss@gmail.com>
2023-08-20 04:16:40 -0300

    nova pagina incluida, curriculo em portugues

--- meu_site.py
+++ meu_site.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+
+@app.route("/curriculo_br")
+def curriculo_br():
+    return render_template('curriculo_br.html')
+
 # colocar o site no ar
 
 if __name__ == "__main__":
